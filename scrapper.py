6611aaab771eb54531c6fcfc1f6fcab65b77d1d1
sriram kalyan <sriramkalyanyandra3@gmail.com>
2022-09-17 19:18:42 +0530

    Successfully created csv output file

--- scrapper.py
+++ scrapper.py
@@ -6,19 +6,30 @@ class Scrapper():
 
     def student_reg_num(self):
         # Getting the title tag
-        print(self.soup.title.text.strip())
         s = self.soup.find('input', id= 'ContentPlaceHolder1_txtRegNo')
-        return s['value']
+        return [s['value']]
 
     def get_subject(self, subject_num='1'):
         s = self.soup.find('span', id= 'ContentPlaceHolder1_dgvStudentHistory_lblPName_{}'.format(subject_num))
         if s is None:
-            return s
-        return s.text
+            return []
+        return [s.text]
 
     def get_subject_marks(self,marks_num='1'):
         s = self.soup.find('span', id= 'ContentPlaceHolder1_dgvStudentHistory_lblGrade_{}'.format(marks_num))
         if s is None:
-            return s
-        return s.text
+            return []
+        return [s.text]
+    
+    def get_sgpa(self):
+        s = self.soup.find('span', id= 'ContentPlaceHolder1_gvSGPA_CGPA_lblSgap_0')
+        if s is None:
+            return []
+        return [s.text]
+
+    def get_cgpa(self):
+        s = self.soup.find('span', id= 'ContentPlaceHolder1_gvSGPA_CGPA_lblCGPA_0')
+        if s is None:
+            return []
+        return [s.text]
     
--- selenium_tut.py
+++ selenium_tut.py
@@ -1,16 +1,17 @@
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from scrapper import Scrapper
 import time
 
 # Keys is used to overwrite the input value of the roll number field instead of appending
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 
 chrome_driver = r'C:\Users\Sriram Kalyan\Documents\Python\csv_scrapper\driver\chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--incognito')
-options.add_argument('--headless')
+# options.add_argument('--headless')
 
 driver = webdriver.Chrome(chrome_driver, options=options)
 
@@ -30,27 +31,34 @@ def get_student_result(regd_no, subject_list):
     print('='*50)
     sc = Scrapper(driver.page_source)
     if not subject_list:
-        subject_list = ['Registerd No']+[
-                sc.get_subject(1),
-                sc.get_subject(2),
-                sc.get_subject(3),
-                sc.get_subject(4),
-                sc.get_subject(5),
-                sc.get_subject(6),
-                sc.get_subject(7),
-                sc.get_subject(8),
-                sc.get_subject(9),
-                sc.get_subject(10)
-            ]+['SGPA', 'CGPA']
-    marks_list = [sc.get_subject_marks(1)]+ [sc.get_subject_marks(2)]+ [sc.get_subject_marks(3)] + \
-                 [sc.get_subject_marks(4)]+ [sc.get_subject_marks(5)]+ [sc.get_subject_marks(6)]+ \
-                 [sc.get_subject_marks(7)]+ [sc.get_subject_marks(8)]+ [sc.get_subject_marks(9)]+ [sc.get_subject_marks(10)]
+        subject_list = ['Registerd No']+sc.get_subject(1)+sc.get_subject(2)+sc.get_subject(3)+ \
+            sc.get_subject(4)+sc.get_subject(5)+sc.get_subject(6)+ sc.get_subject(7)+ \
+            sc.get_subject(8)+sc.get_subject(9)+sc.get_subject(10)+['SGPA', 'CGPA']
+    
+    marks_list = sc.student_reg_num() +sc.get_subject_marks(1)+ sc.get_subject_marks(2)+ \
+                 sc.get_subject_marks(3) + sc.get_subject_marks(4) + sc.get_subject_marks(5)+ \
+                 sc.get_subject_marks(6) + sc.get_subject_marks(7) + sc.get_subject_marks(8) + sc.get_subject_marks(9)+ \
+                 sc.get_subject_marks(10) + sc.get_sgpa() + sc.get_cgpa()
 
     return subject_list, marks_list
 
-sample_student_list = ['21b91a04q2','21b91a04q3','21b91a04q4']
-subjects = []
-for regd_num in sample_student_list:
-    subjects, marks_list = get_student_result(regd_num, subjects)
-    print(marks_list)
+with open('students_list.txt','r') as input_file:
+    # output_csv_filename = input('Enter the output csv filename : ') + '.csv'
+    output_csv_filename = 'output.csv'
+    subjects = []
+
+    with open(output_csv_filename,'w', newline='', encoding='utf-8') as output_file:
+        csv_writer = csv.writer(output_file)
+
+        is_row_header = True
+
+        for regd_num in input_file.readlines():
+            subjects, marks_list = get_student_result(regd_num.rstrip(), subjects)
+            if is_row_header:
+                csv_writer.writerow(subjects)
+                is_row_header = False
+                
+            csv_writer.writerow(marks_list)
+                
+            print(marks_list)
 

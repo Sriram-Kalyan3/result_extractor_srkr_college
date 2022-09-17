de09b4d1a6a43e93e9da7975e386914178e53629
sriram kalyan <sriramkalyanyandra3@gmail.com>
2022-09-17 17:45:45 +0530

    Able to get marks of students in the subject order

--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,24 @@
-import requests
 from bs4 import BeautifulSoup
 
+class Scrapper():
+    def __init__(self,pagesource):
+        self.soup = BeautifulSoup(pagesource,features="html.parser")
 
-# Making a GET request
-r = requests.get('http://www.srkrexams.in/Result.aspx?Id=2402')
+    def student_reg_num(self):
+        # Getting the title tag
+        print(self.soup.title.text.strip())
+        s = self.soup.find('input', id= 'ContentPlaceHolder1_txtRegNo')
+        return s['value']
 
-# Parsing the HTML
-soup = BeautifulSoup(r.content, 'html.parser')
+    def get_subject(self, subject_num='1'):
+        s = self.soup.find('span', id= 'ContentPlaceHolder1_dgvStudentHistory_lblPName_{}'.format(subject_num))
+        if s is None:
+            return s
+        return s.text
 
-# Getting the title tag
-print(soup.title.text.strip())
-
-print(soup)
-
-'''
-ContentPlaceHolder1_txtRegNo
-'''
-s = soup.find('input', id= 'ContentPlaceHolder1_txtRegNo')
-print(s)
+    def get_subject_marks(self,marks_num='1'):
+        s = self.soup.find('span', id= 'ContentPlaceHolder1_dgvStudentHistory_lblGrade_{}'.format(marks_num))
+        if s is None:
+            return s
+        return s.text
+    
--- selenium_tut.py
+++ selenium_tut.py
@@ -1,16 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from scrapper import Scrapper
 import time
 
+# Keys is used to overwrite the input value of the roll number field instead of appending
+from selenium.webdriver.common.keys import Keys
+
 chrome_driver = r'C:\Users\Sriram Kalyan\Documents\Python\csv_scrapper\driver\chromedriver.exe'
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
 
-driver = webdriver.Chrome(chrome_driver)
-driver.get('http://www.srkrexams.in/Result.aspx?Id=2402')
-time.sleep(2)
+driver = webdriver.Chrome(chrome_driver, options=options)
 
-regd_number = driver.find_element(By.NAME,'ctl00$ContentPlaceHolder1$txtRegNo')
-getresult_button = driver.find_element(By.XPATH,'//*[@id="ContentPlaceHolder1_btnGetResult"]')
 
-regd_number.send_keys('21b91a04q2')
-getresult_button.click()
-print('#'*30)
+def get_student_result(regd_no, subject_list):
+    driver.get('http://www.srkrexams.in/Result.aspx?Id=2402')
+    time.sleep(2)
+
+    #Input field for ROLL NUMBER
+    regd_number_webElement = driver.find_element(By.NAME,'ctl00$ContentPlaceHolder1$txtRegNo')
+    #GET RESULT button
+    getresult_button = driver.find_element(By.XPATH,'//*[@id="ContentPlaceHolder1_btnGetResult"]')
+
+    # regd_number_webElement.sendKeys(Keys.chord(Keys.CONTROL, "a"), regd_no)
+    regd_number_webElement.send_keys(regd_no)
+    getresult_button.click()
+    print('='*50)
+    sc = Scrapper(driver.page_source)
+    if not subject_list:
+        subject_list = ['Registerd No']+[
+                sc.get_subject(1),
+                sc.get_subject(2),
+                sc.get_subject(3),
+                sc.get_subject(4),
+                sc.get_subject(5),
+                sc.get_subject(6),
+                sc.get_subject(7),
+                sc.get_subject(8),
+                sc.get_subject(9),
+                sc.get_subject(10)
+            ]+['SGPA', 'CGPA']
+    marks_list = [sc.get_subject_marks(1)]+ [sc.get_subject_marks(2)]+ [sc.get_subject_marks(3)] + \
+                 [sc.get_subject_marks(4)]+ [sc.get_subject_marks(5)]+ [sc.get_subject_marks(6)]+ \
+                 [sc.get_subject_marks(7)]+ [sc.get_subject_marks(8)]+ [sc.get_subject_marks(9)]+ [sc.get_subject_marks(10)]
+
+    return subject_list, marks_list
+
+sample_student_list = ['21b91a04q2','21b91a04q3','21b91a04q4']
+subjects = []
+for regd_num in sample_student_list:
+    subjects, marks_list = get_student_result(regd_num, subjects)
+    print(marks_list)
+

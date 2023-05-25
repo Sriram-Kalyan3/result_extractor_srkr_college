d880b85be4b79ec079484d7c56d70641683a415d
sriram <sriramkalyanyandra3@gmail.com>
2023-05-25 14:02:52 +0530

    Updated code such that open elective subjects

--- scrapper.py
+++ scrapper.py
@@ -1,35 +1,35 @@
 from bs4 import BeautifulSoup
 
+
 class Scrapper():
-    def __init__(self,pagesource):
-        self.soup = BeautifulSoup(pagesource,features="html.parser")
+    def __init__(self, pagesource):
+        self.soup = BeautifulSoup(pagesource, features="html.parser")
 
     def student_reg_num(self):
         # Getting the title tag
-        s = self.soup.find('input', id= 'ContentPlaceHolder1_txtRegNo')
+        s = self.soup.find('input', id='ContentPlaceHolder1_txtRegNo')
         return [s['value']]
 
     def get_subject(self, subject_num='1'):
-        s = self.soup.find('span', id= 'ContentPlaceHolder1_dgvStudentHistory_lblPName_{}'.format(subject_num))
+        s = self.soup.find('span', id='ContentPlaceHolder1_dgvStudentHistory_lblPName_{}'.format(subject_num))
         if s is None:
-            return ['']
+            return []
         return [s.text]
 
-    def get_subject_marks(self,marks_num='1'):
-        s = self.soup.find('span', id= 'ContentPlaceHolder1_dgvStudentHistory_lblGrade_{}'.format(marks_num))
+    def get_subject_marks(self, marks_num='1'):
+        s = self.soup.find('span', id='ContentPlaceHolder1_dgvStudentHistory_lblGrade_{}'.format(marks_num))
         if s is None:
-            return ['']
+            return []
         return [s.text]
-    
+
     def get_sgpa(self):
-        s = self.soup.find('span', id= 'ContentPlaceHolder1_gvSGPA_CGPA_lblSgap_0')
+        s = self.soup.find('span', id='ContentPlaceHolder1_gvSGPA_CGPA_lblSgap_0')
         if s is None:
             return ['']
         return [s.text]
 
     def get_cgpa(self):
-        s = self.soup.find('span', id= 'ContentPlaceHolder1_gvSGPA_CGPA_lblCGPA_0')
+        s = self.soup.find('span', id='ContentPlaceHolder1_gvSGPA_CGPA_lblCGPA_0')
         if s is None:
             return []
         return [s.text]
-    
